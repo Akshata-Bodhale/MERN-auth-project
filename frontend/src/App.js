562110eb11a33c1875b1ce382085d3a1f1f4f6fc
Akshata-Bodhale <akshatabodhale22@gmail.com>
2026-04-29 22:22:04 +0530

    MERN Auth Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
-import './styles.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
